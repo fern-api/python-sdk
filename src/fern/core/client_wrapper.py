56b16844c24a5540fb650be56cdb08a78a004175
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-03-15 00:58:53 +0000

    Release 0.0.74

--- src/fern/core/client_wrapper.py
+++ src/fern/core/client_wrapper.py
@@ -16,7 +16,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "fern-api",
-            "X-Fern-SDK-Version": "0.0.73",
+            "X-Fern-SDK-Version": "0.0.74",
         }
         headers["Authorization"] = f"Bearer {self._get_token()}"
         return headers
